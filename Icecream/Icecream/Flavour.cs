85e2cc8e15760713d239f8c6260b7dff6d25704e
eaaxsnn <easonisgey@gmail.com>
2024-01-08 11:09:04 +0800

    Adding classes

---
+++ Icecream/Icecream/Flavour.cs
@@ -0,0 +1,22 @@
+﻿namespace Icecream;
+
+public class Flavour
+{
+    public string Type { get; set; }
+    public bool Premium { get; set; }
+    public int Quantity { get; set; }
+    
+    public Flavour(){}
+
+    public Flavour(string type, bool premium, int quantity)
+    {
+        Type = type;
+        Premium = premium;
+        Quantity = quantity;
+    }
+
+    public override string ToString()
+    {
+        return $"Type: {Type} | Premium: {Premium} | Quantity: {Quantity}";
+    }
+}
---
+++ Icecream/Icecream/Order.cs
@@ -0,0 +1,33 @@
+﻿namespace Icecream;
+
+public class Order
+{
+    public int Id { get; set; }
+    public DateTime TimeReceived { get; set; }
+    public DateTime? TimeFulfilled { get; set; } = new DateTime();
+    public List<IceCream> IceCreamList { get; set; } = new List<IceCream>();
+    
+    public Order(){}
+
+    public Order(int id, DateTime timeReceived)
+    {
+        Id = id;
+        TimeReceived = timeReceived;
+    }
+
+    void ModifyIceCream(int)
+    {
+        
+    }
+
+    public void AddIceCream(IceCream ic)
+    {
+        IceCreamList.Add(ic);
+    }
+
+    public void DeleteIceCream(int index)
+    {
+        IceCreamList.RemoveAt(index);
+    }
+    
+}
